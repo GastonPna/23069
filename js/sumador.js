f16e0b3d7073b50835fa4ff696871b756ed2b4e8
GastonPna <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>
2023-06-02 20:45:46 -0300

    comenzamos javascript

---
+++ js/sumador.js
@@ -0,0 +1,7 @@
+//alert('Hola mundo');
+
+const valor1 = parseInt(prompt('Ingrese el valor 1'));
+const valor2= parseInt(prompt('Ingrese el valor 2'));
+alert('El resultado de la suma es: '+(valor1+valor2));
+//const resultado = valor1 + valor2;
+//console.log(resultado);
